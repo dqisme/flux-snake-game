50133809a45d8d8cf526d92c457a24dddac23d5f
dqisme <dqisme@outlook.com>
2019-04-19 16:00:24 +0800

    [dq] create a basic grid.

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Container } from 'flux/utils';
 
 import SnakeStore from '../../data/SnakeStore';
-import logo from '../../logo.svg';
-import './App.css';
 import FluxContainerConvert from '../../utils/FluxContainerConvert';
 
 class App extends Component {
@@ -17,22 +15,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div data-testid="grid"></div>
     );
   }
 }
--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { render } from 'react-testing-library';
 import App from './App';
 
-it('renders welcome message', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('Learn React')).toBeInTheDocument();
+describe('App', function () {
+  it('renders a grid', () => {
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('grid')).toBeInTheDocument();
+  });
 });
